09ca79ffd9dea420c8a2ac56a613d0afe6af1605
Fredrik Wärnsberg <fredrik@warnsberg.se>
2019-06-25 14:37:44 +0200

    Add initial collector

--- lib/prometheus_hackney_collector.ex
+++ lib/prometheus_hackney_collector.ex
@@ -1,18 +1,47 @@
 defmodule PrometheusHackneyCollector do
-  @moduledoc """
-  Documentation for PrometheusHackneyCollector.
-  """
+  use Prometheus.Collector
 
-  @doc """
-  Hello world.
+  @metrics [:max, :in_use_count, :free_count, :queue_count]
 
-  ## Examples
+  def install() do
+    require Prometheus.Registry
+    Prometheus.Registry.register_collector(__MODULE__)
+  end
 
-      iex> PrometheusHackneyCollector.hello()
-      :world
+  def collect_mf(_registry, callback) do
+    pools = for {pool, _pid} <- get_pools(), into: %{} do
+      {pool, :hackney_pool.get_stats(pool)}
+    end
 
-  """
-  def hello do
-    :world
+    for m <- @metrics do
+      values = for {pool, metrics} <- pools do
+        {^m, value} = List.keyfind(metrics, m, 0)
+        {[pool: pool], value}
+      end
+
+      callback.(create_gauge(
+            to_name(m),
+            "The total number of #{inspect m} per Hackney pool.",
+            values
+          ))
+    end
+    :ok
+  end
+
+  def collect_metrics(_metric, values) do
+    Prometheus.Model.gauge_metrics(values)
+  end
+
+  defp create_gauge(name, help, values) do
+    Prometheus.Model.create_mf(name, help, :gauge, __MODULE__, values)
+  end
+
+  defp get_pools() do
+    # Hack.
+    :ets.tab2list(:hackney_pool)
+  end
+
+  defp to_name(metric) do
+    [:hackney_pool, metric, :count] |> Enum.join("_") |> String.to_atom()
   end
 end
