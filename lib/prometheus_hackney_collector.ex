72d573f88dbe8f17caa5f50940417d3c84eb6183
Fredrik Wärnsberg <fredrik@warnsberg.se>
2019-06-25 14:23:51 +0200

    Project setup

---
+++ lib/prometheus_hackney_collector.ex
@@ -0,0 +1,18 @@
+defmodule PrometheusHackneyCollector do
+  @moduledoc """
+  Documentation for PrometheusHackneyCollector.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PrometheusHackneyCollector.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/prometheus_hackney_collector_test.exs
@@ -0,0 +1,8 @@
+defmodule PrometheusHackneyCollectorTest do
+  use ExUnit.Case
+  doctest PrometheusHackneyCollector
+
+  test "greets the world" do
+    assert PrometheusHackneyCollector.hello() == :world
+  end
+end
