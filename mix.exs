d037e90cda68a654eeadf1b3022fcbbb40f5ce6d
Fredrik Wärnsberg <fredrik@warnsberg.se>
2019-06-25 14:42:06 +0200

    0.1.0

--- mix.exs
+++ mix.exs
@@ -5,9 +5,11 @@ defmodule PrometheusHackneyCollector.MixProject do
     [
       app: :prometheus_hackney_collector,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/soundtrackyourbrand/prometheus-hackney-collector"
     ]
   end
 
@@ -18,11 +20,21 @@ defmodule PrometheusHackneyCollector.MixProject do
     ]
   end
 
+  def package do
+    [
+      name: "prometheus_hackney_collector",
+      files: ~w(lib .formatter mix.exs README* LICENSE*)
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/soundtrackyourbrand/prometheus-hackney-collector"
+      }
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:hackney, "~> 1.15"},
+      {:prometheus, "~> 4.0"}
     ]
   end
 end
